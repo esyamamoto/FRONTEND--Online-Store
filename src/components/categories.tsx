82b4b7369729cb0444ee671bb1d491e779382281
FabianoDaLuz <escottofabiano@gmail.com>
2023-08-21 18:05:00 -0300

    req5 halfdone

---
+++ src/components/categories.tsx
@@ -0,0 +1,37 @@
+import { useEffect, useState } from 'react';
+import * as api from '../services/api';
+
+type Categorie = {
+  id: string;
+  name: string;
+};
+
+function CategoriesList() {
+  const [categories, setCategories] = useState<Categorie[]>([]);
+
+  useEffect(() => {
+    async function gCategories() {
+      const response = await api.getCategories();
+      setCategories(response);
+    }
+    gCategories();
+  }, []);
+  // console.log(categories);
+
+  return (
+    <>
+      <h2>Categorias:</h2>
+      <ul>
+        {categories.map((cat) => (
+          <li key={ cat.id }>
+            <label data-testid="category" htmlFor={ cat.id }>
+              <input type="radio" name="categorie" id={ cat.id } value={ cat.id } />
+              {cat.name}
+            </label>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
+export default CategoriesList;
